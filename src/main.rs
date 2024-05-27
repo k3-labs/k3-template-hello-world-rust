adae12d31a0c1e59ac0f9328bd4ae68551dafcfb
Levike34 <89289513+Levike34@users.noreply.github.com>
2024-05-27 11:56:03 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,6 @@ use k3_wasm_sdk::http::{Request, Response};
 
 #[http_handler]
 pub fn get(_req: Request<Vec<u8>>) -> Response<Vec<u8>> {
-    let _env_var = std::env::var("TEST_ENV_2").expect("TEST_ENV_2 variable must be set");
-
     Response::builder()
         .status(200)
         .body(
