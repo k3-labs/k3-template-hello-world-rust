560236d0f84f3ec0daa93b5680b7557db1014ec2
Levike34 <89289513+Levike34@users.noreply.github.com>
2024-06-19 10:49:10 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use k3_wasm_macros::http_handler;
 use k3_wasm_sdk::http::{Request, Response};
-use k3_wasm_sdk::{ENV_VAR_LEN, ENV_VAR_PTR};
 
 #[http_handler]
 pub fn get(_req: Request<Vec<u8>>) -> Response<Vec<u8>> {
