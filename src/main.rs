3bf2e9ac279c0a90588a890e5e0b6cb2b39d0cff
Levike34 <89289513+Levike34@users.noreply.github.com>
2024-08-19 21:25:40 +0900

    response to json

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,22 @@
-use k3_wasm_macros::http_handler;
+use k3_wasm_macros::{define_k3_write_inputs, http_handler};
 use k3_wasm_sdk::http::{Request, Response};
+use serde_json::*;
 
 #[http_handler]
 pub fn get(_req: Request<Vec<u8>>) -> Response<Vec<u8>> {
+    // Create a JSON object
+    let response_json = json!({
+        "response": "Hello world from K3"
+    });
+
+    // Convert JSON object to a string and then to bytes
+    let response_body = response_json.to_string().into_bytes();
+
+    // Build the response
     Response::builder()
         .status(200)
-        .body(
-            "Hello world from K3"
-                .as_bytes()
-                .to_vec(),
-        )
+        .header("Content-Type", "application/json")
+        .body(response_body)
         .unwrap()
 }
 
